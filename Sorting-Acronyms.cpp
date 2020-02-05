f7a92723c3bfa83320398f12dcf2862fc7de2732
thanawatgulati <thanawatgulatijames@gmail.com>
2020-02-05 22:20:59 +0700

    🎖 fix

--- Sorting-Acronyms.cpp
+++ Sorting-Acronyms.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -17,18 +18,22 @@ void print_word(int n)
 {
     for (int i = 0; i < n; i++)
     {
-        char x = v_word[i][0];
-        if (x >= 65 && x <= 90)
+        string sub = v_word[i].find(' ');
+        // for (int j = 0; j < (v_word[i].split(' ')).size(); j++)
         {
-            cout << v_word[i][0];
+            char x = v_word[i][0];
+            if (x >= 65 && x <= 90)
+            {
+                cout << v_word[i][0];
+            }
         }
+        // }
+        cout << endl;
     }
-    cout << endl;
-}
-int main()
-{
-    int n;
-    cin >> n;
-    input_word(n);
-    print_word(n);
-}
+    int main()
+    {
+        int n;
+        cin >> n;
+        input_word(n);
+        print_word(n);
+    }
